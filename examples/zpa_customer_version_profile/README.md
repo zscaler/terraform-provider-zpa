66d08a4fa78edc9587dc1456292b78b0b2973d47
William Guilherme <wguilherme@securitygeek.io>
2021-10-30 23:35:50 -0700

    updated `customer_version_profile` datasource example

---
+++ examples/zpa_customer_version_profile/README.md
@@ -0,0 +1,16 @@
+# Retrieve Customer Version Profile
+
+This example will show you how to retrieve the customer version profile to attach to a ZPA App Connector Group.
+This example codifies [this API](https://help.zscaler.com/zpa/api-reference#/customer-version-profile-controller).
+
+To run, configure your ZPA provider as described [Here](https://github.com/SecurityGeekIO/terraform-provider-zpa/blob/master/website/docs/index.html.markdown)
+
+## Run the example
+
+From inside of this directory:
+
+```bash
+terraform init
+terraform plan -out theplan
+terraform apply theplan
+```
---
+++ examples/zpa_customer_version_profile/datasource.tf
@@ -0,0 +1,27 @@
+// Retrieve "Default" customer version profile
+data "zpa_customer_version_profile" "default"{
+    name = "Default"
+}
+
+output "zpa_customer_version_profile_default" {
+    value = data.zpa_customer_version_profile.default
+}
+
+// Retrieve "Previous Default" customer version profile
+data "zpa_customer_version_profile" "previous_default"{
+    name = "Previous Default"
+}
+
+output "zpa_customer_version_profile_previous_default" {
+    value = data.zpa_customer_version_profile.previous_default
+}
+
+// Retrieve "New Release" customer version profile
+data "zpa_customer_version_profile" "new_release"{
+    name = "New Release"
+}
+
+output "zpa_customer_version_profile_new_release" {
+    value = data.zpa_customer_version_profile.new_release
+}
+
