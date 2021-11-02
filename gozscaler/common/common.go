aaa21edaafa03d0175032533cbc5e73c0e9dd029
amazzalel-habib <amazzal.elhabib@gmail.com>
2021-11-02 18:15:55 +0100

    fix search field

--- gozscaler/common/common.go
+++ gozscaler/common/common.go
@@ -3,5 +3,5 @@ package common
 type Pagination struct {
 	PageSize int    `json:"pagesize"`
 	Page     int    `json:"page,omitempty"`
-	Search   string `json:"Search,omitempty"`
+	Search   string `json:"search,omitempty"`
 }
