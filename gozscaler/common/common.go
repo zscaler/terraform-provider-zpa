5a610104cc61f097d7d526b977c311553f7d5ac7
William Guilherme <wguilherme@securitygeek.io>
2021-12-20 11:27:11 -0800

    Updated search parameter in commong.go

--- gozscaler/common/common.go
+++ gozscaler/common/common.go
@@ -7,9 +7,5 @@ const (
 type Pagination struct {
 	PageSize int    `json:"pagesize" url:"pagesize"`
 	Page     int    `json:"page,omitempty" url:"page"`
-<<<<<<< HEAD
 	Search   string //`json:"search,omitempty" url:"search"`
-=======
-	Search   string `json:"-" url:"-"`
->>>>>>> 615856b1a9082873acb1096923c7c71d4d91a39f
 }
