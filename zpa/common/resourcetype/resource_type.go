bf3dc52070324cd6f12a2b8927e64dc0ed24938f
William Guilherme <wguilherme@securitygeek.io>
2022-04-27 19:25:46 -0700

    🆕 zpa#60: New Policy Access Timeout Rule Acceptance Test  (#99)
     zpa#60: New Policy Access Timeout Rule Acceptance Test

--- zpa/common/resourcetype/resource_type.go
+++ zpa/common/resourcetype/resource_type.go
@@ -9,11 +9,8 @@ const (
 	ZPAServerGroup            = "zpa_server_group"
 	ZPASegmentGroup           = "zpa_segment_group"
 	ZPAApplicationSegment     = "zpa_application_segment"
-<<<<<<< HEAD
-	ZPAPolicyType             = "zpa_policy_type"
-=======
 	ZPABrowserAccess          = "zpa_browser_access"
->>>>>>> master
+	ZPAPolicyType             = "zpa_policy_type"
 	ZPAPolicyAccessRule       = "zpa_policy_access_rule"
 	ZPAPolicyTimeOutRule      = "zpa_policy_timeout_rule"
 	ZPAPolicyForwardingRule   = "zpa_policy_forwarding_rule"
