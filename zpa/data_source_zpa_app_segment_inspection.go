27c6d11384503791ab0c33ed2d3d0df901989202
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-05-19 16:52:52 -0700

    fix go mod

--- zpa/data_source_zpa_app_segment_inspection.go
+++ zpa/data_source_zpa_app_segment_inspection.go
@@ -5,7 +5,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/willguibr/terraform-provider-zpa/gozscaler/appsegment_inspection"
+	"github.com/zscaler/terraform-provider-zpa/gozscaler/appsegment_inspection"
 )
 
 func dataSourceAppSegmentInspection() *schema.Resource {
