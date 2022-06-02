1ff5179d36ee53a599da3e3a12e7551cde2af60d
amazzalel-habib <amazzal.elhabib@gmail.com>
2022-06-02 23:31:11 +0100

    fix beta cloud auth & load credentials from config file as last resort

--- gozscaler/config.go
+++ gozscaler/config.go
@@ -2,10 +2,15 @@ package gozscaler
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 	"sync"
 	"time"
@@ -15,14 +20,15 @@ import (
 )
 
 const (
-	defaultBaseURL    = "https://config.private.zscaler.com"
-	betaBaseURL       = "https://config.zpabeta.net"
-	defaultTimeout    = 240 * time.Second
-	loggerPrefix      = "zpa-logger: "
-	ZPA_CLIENT_ID     = "ZPA_CLIENT_ID"
-	ZPA_CLIENT_SECRET = "ZPA_CLIENT_SECRET"
-	ZPA_CUSTOMER_ID   = "ZPA_CUSTOMER_ID"
-	ZPA_CLOUD         = "ZPA_CLOUD"
+	defaultBaseURL           = "https://config.private.zscaler.com"
+	betaBaseURL              = "https://config.zpabeta.net"
+	defaultTimeout           = 240 * time.Second
+	loggerPrefix             = "zpa-logger: "
+	ZPA_CLIENT_ID            = "ZPA_CLIENT_ID"
+	ZPA_CLIENT_SECRET        = "ZPA_CLIENT_SECRET"
+	ZPA_CUSTOMER_ID          = "ZPA_CUSTOMER_ID"
+	ZPA_CLOUD                = "ZPA_CLOUD"
+	configPath        string = ".zpa/credentials.json"
 )
 
 type BackoffConfig struct {
@@ -35,6 +41,12 @@ type AuthToken struct {
 	TokenType   string `json:"token_type"`
 	AccessToken string `json:"access_token"`
 	//ExpiresIn   string `json:"expires_in"`
+}
+type CredentialsConfig struct {
+	ClientID     string `json:"zpa_client_id"`
+	ClientSecret string `json:"zpa_client_secret"`
+	CustomerID   string `json:"zpa_customer_id"`
+	ZpaCloud     string `json:"zpa_cloud"`
 }
 
 // Config contains all the configuration data for the API client
@@ -59,25 +71,36 @@ By default it will try to read the access and te secret from the environment var
 // 20 times in a 10 second interval for a GET call.
 // 10 times in a 10 second interval for any POST/PUT/DELETE call.
 // TODO Add healthCheck method to NewConfig
-func NewConfig(clientID, clientSecret, customerID, rawUrl string) (*Config, error) {
+func NewConfig(clientID, clientSecret, customerID, cloud string) (*Config, error) {
 	backoffConf := &BackoffConfig{
 		Enabled:             true,
 		MaxNumOfRetries:     100,
 		RetryWaitMaxSeconds: 20,
 		RetryWaitMinSeconds: 5,
 	}
+	// if creds not provided in TF config, try loading from env vars
 	if clientID == "" || clientSecret == "" || customerID == "" {
 		clientID = os.Getenv(ZPA_CLIENT_ID)
 		clientSecret = os.Getenv(ZPA_CLIENT_SECRET)
 		customerID = os.Getenv(ZPA_CUSTOMER_ID)
 	}
-	if rawUrl == "" {
-		cloud := os.Getenv(ZPA_CLOUD)
-		if strings.EqualFold(cloud, "BETA") {
-			rawUrl = betaBaseURL
-		} else {
-			rawUrl = defaultBaseURL
+	// last resort to configuration file:
+	if clientID == "" || clientSecret == "" || customerID == "" {
+		creds, err := loadCredentialsFromConfig()
+		if err != nil || creds == nil {
+			return nil, err
 		}
+		clientID = creds.ClientID
+		clientSecret = creds.ClientSecret
+		customerID = creds.CustomerID
+		cloud = creds.ZpaCloud
+	}
+	rawUrl := defaultBaseURL
+	if cloud == "" {
+		cloud = os.Getenv(ZPA_CLOUD)
+	}
+	if strings.EqualFold(cloud, "BETA") {
+		rawUrl = betaBaseURL
 	}
 
 	var logger *log.Logger
@@ -98,6 +121,28 @@ func NewConfig(clientID, clientSecret, customerID, rawUrl string) (*Config, erro
 		CustomerID:   customerID,
 		BackoffConf:  backoffConf,
 	}, err
+}
+
+// loadCredentialsFromConfig Returns the credentials found in a config file
+func loadCredentialsFromConfig() (*CredentialsConfig, error) {
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+	path := filepath.Join(dir, configPath)
+	log.Printf("[INFO]Loading confiuration file at:%s", path)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, errors.New("Could not open credentials file, needs to contain one json object with keys: zpa_client_id, zpa_client_secret, zpa_customer_id, and zpa_cloud. " + err.Error())
+	}
+	configBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	var config CredentialsConfig
+	err = json.Unmarshal(configBytes, &config)
+	if err != nil || config.ClientID == "" || config.ClientSecret == "" || config.CustomerID == "" || config.ZpaCloud == "" {
+		return nil, errors.New("Could not parse credentials file, needs to contain one json object with keys: zpa_client_id, zpa_client_secret, zpa_customer_id, and zpa_cloud. " + err.Error())
+	}
+	return &config, nil
 }
 
 func (c *Config) GetHTTPClient() *http.Client {
