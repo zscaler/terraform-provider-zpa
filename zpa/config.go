a3779852133e8ed020b582168322325ee4302991
amazzalel-habib <amazzal.elhabib@gmail.com>
2022-06-18 00:37:16 +0100

    fix scim attribute & get its values and validate scim operand in policy access rules..

--- zpa/config.go
+++ zpa/config.go
@@ -24,7 +24,6 @@ import (
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/samlattribute"
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/scimattributeheader"
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/scimgroup"
-	"github.com/zscaler/terraform-provider-zpa/gozscaler/scimuserattributeheader"
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/segmentgroup"
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/servergroup"
 	"github.com/zscaler/terraform-provider-zpa/gozscaler/serviceedgecontroller"
@@ -57,7 +56,6 @@ type Client struct {
 	samlattribute                samlattribute.Service
 	scimgroup                    scimgroup.Service
 	scimattributeheader          scimattributeheader.Service
-	scimuserattributeheader      scimuserattributeheader.Service
 	segmentgroup                 segmentgroup.Service
 	servergroup                  servergroup.Service
 	serviceedgegroup             serviceedgegroup.Service
@@ -99,7 +97,6 @@ func (c *Config) Client() (*Client, error) {
 		samlattribute:                *samlattribute.New(config),
 		scimgroup:                    *scimgroup.New(config),
 		scimattributeheader:          *scimattributeheader.New(config),
-		scimuserattributeheader:      *scimuserattributeheader.New(config),
 		segmentgroup:                 *segmentgroup.New(config),
 		servergroup:                  *servergroup.New(config),
 		serviceedgegroup:             *serviceedgegroup.New(config),
