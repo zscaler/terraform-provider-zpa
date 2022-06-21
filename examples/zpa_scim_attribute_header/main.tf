29de600042e6c3bc679942127d90639c1cfa0686
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-06-20 20:55:08 -0700

    Updated Index.md and added scim-attribute_header document

--- examples/zpa_scim_attribute_header/main.tf
+++ examples/zpa_scim_attribute_header/main.tf
@@ -1,8 +1,14 @@
 data "zpa_scim_attribute_header" "email_value" {
     name = "emails.value"
-    idp_name = "idp_name"
+    idp_name = "IdP_Name"
 }
 
-output "get_zpa_scim_attribute_header" {
-  value = data.zpa_scim_attribute_header.email_value
+data "zpa_scim_attribute_header" "givenName" {
+  name     = "name.givenName"
+  idp_name = "IdP_Name"
+}
+
+data "zpa_scim_attribute_header" "familyName" {
+  name     = "name.familyName"
+  idp_name = "IdP_Name"
 }
