46600947f8b34589b64bde0aa6916a487c5092d3
William Guilherme <wguilherme@zscaler.com>
2022-06-30 17:08:24 -0700

    New: Added support to zpagov.net #144 (#145)
    * New: Added support to zpagov.net #144
    
    * Updated provider.go

--- zpa/provider.go
+++ zpa/provider.go
@@ -34,9 +34,8 @@ func Provider() *schema.Provider {
 			"zpa_cloud": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				DefaultFunc:  envDefaultFunc("ZPA_CLOUD"),
-				Description:  "Cloud to use BETA or PRODUCTION",
-				ValidateFunc: validation.StringInSlice([]string{"BETA", "PRODUCTION"}, true),
+				Description:  "Cloud to use PRODUCTION, BETA, GOV",
+				ValidateFunc: validation.StringInSlice([]string{"PRODUCTION", "BETA", "GOV"}, true),
 				Default:      "PRODUCTION",
 			},
 		},
