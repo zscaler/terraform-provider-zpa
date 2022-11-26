b547a4324ab99f42c1b1fa94f9c6b1ffe236b4fd
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-11-26 14:30:11 -0800

    Updated GNUmkefile and CHANGELOG

--- zpa/resource_zpa_segment_group.go
+++ zpa/resource_zpa_segment_group.go
@@ -127,13 +127,12 @@ func resourceSegmentGroupRead(d *schema.ResourceData, m interface{}) error {
 
 	log.Printf("[INFO] Getting segment group:\n%+v\n", resp)
 	d.SetId(resp.ID)
-	tcpKeepAliveEnabled, _ := strconv.ParseBool(resp.TcpKeepAliveEnabled)
 	_ = d.Set("config_space", resp.ConfigSpace)
 	_ = d.Set("description", resp.Description)
 	_ = d.Set("enabled", resp.Enabled)
 	_ = d.Set("name", resp.Name)
 	_ = d.Set("policy_migrated", resp.PolicyMigrated)
-	_ = d.Set("tcp_keep_alive_enabled", tcpKeepAliveEnabled)
+	_ = d.Set("tcp_keep_alive_enabled", resp.TcpKeepAliveEnabled)
 	if err := d.Set("applications", flattenSegmentGroupApplicationsSimple(resp)); err != nil {
 		return fmt.Errorf("failed to read applications %s", err)
 	}
