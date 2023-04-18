f655a33bc73912dee10daf624fa37388aac766fb
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-04-17 21:20:14 -0700

    Added Computed attribute to app segment srv group

--- zpa/resource_zpa_application_segment.go
+++ zpa/resource_zpa_application_segment.go
@@ -196,6 +196,7 @@ func resourceApplicationSegment() *schema.Resource {
 			"server_groups": {
 				Type:        schema.TypeSet,
 				Optional:    true,
+				Computed:    true,
 				Description: "List of the server group IDs.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
